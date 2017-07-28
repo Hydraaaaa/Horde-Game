1f31e3bfd57a395ecafea44d6165a5c2c8b4d167
James da Silva <james.dasilva1111@gmail.com>
2017-07-28 15:33:18 +1000

    Fixes

--- Assets/Scripts/PlayerInit.cs
+++ Assets/Scripts/PlayerInit.cs
@@ -7,10 +7,7 @@ public class PlayerInit : MonoBehaviour
 	void Start ()
     {
         Transform camera = transform.GetChild(0);
-        camera.GetComponent<CameraMovement>().player = gameObject;
         camera.SetParent(null);
-
-        GetComponent<PlayerMovement>().camera = camera.gameObject;
 
         Destroy(this);
 	}
