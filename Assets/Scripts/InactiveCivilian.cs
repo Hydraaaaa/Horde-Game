7bc0345c02abeb973c1891f142235d3acd1966b1
James da Silva <james.dasilva1111@gmail.com>
2017-08-03 09:57:39 +1000

    Added inactive civilians
    Inactive civilians are lost civilians that will begin pathing to the exit once found by a player

---
+++ Assets/Scripts/InactiveCivilian.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InactiveCivilian : MonoBehaviour
+{
+    public Material inactiveMaterial;
+    public Material activeMaterial;
+
+    CivilianNavigation navigationScript;
+    Renderer renderer;
+
+    public float range;
+
+	void Start ()
+    {
+        navigationScript = GetComponent<CivilianNavigation>();
+        navigationScript.enabled = false;
+
+        renderer = GetComponent<Renderer>();
+        renderer.material = inactiveMaterial;
+	}
+	
+	void Update ()
+    {
+        for (int i = 0; i < navigationScript.gameObjectManager.players.Count; i++)
+        {
+            int mask = ~(1 << 9);
+            if (Vector3.Distance(transform.position, navigationScript.gameObjectManager.players[i].transform.position) <= range)
+            {
+                if (!Physics.Linecast(transform.position, navigationScript.gameObjectManager.players[i].transform.position, mask))
+                {
+                    Debug.Log("Freed");
+                    renderer.material = activeMaterial;
+                    navigationScript.enabled = true;
+                }
+            }
+        }
+	}
+}
