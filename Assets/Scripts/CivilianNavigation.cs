3cf608f042f1fb20eb07c22220e0e6e56e22adc0
Josh <joshuacorney2@gmail.com>
2017-08-02 16:34:15 +1000

    Merge branch 'master' of https://github.com/Hydraaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa/Horde-Game

---
+++ Assets/Scripts/CivilianNavigation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(UnityEngine.AI.NavMeshAgent))]
+public class CivilianNavigation : MonoBehaviour
+{
+    public GameObjectManager gameObjectManager;
+    UnityEngine.AI.NavMeshAgent agent;
+
+    public float speed;
+
+	void Start ()
+    {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        agent.speed = speed;
+	}
+	
+	void Update ()
+    {
+        if (gameObjectManager != null)
+        {
+            agent.SetDestination(gameObjectManager.endPos.transform.position);
+        }
+	}
+}
---
+++ Assets/Scripts/CivilianSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CivilianSpawner : MonoBehaviour
+{
+    [HideInInspector]
+    public GameObjectManager gameObjectManager;
+    public GameObject spawnPrefab;
+    
+    public int amount;
+
+    void Start ()
+    {
+        for (int i = 0; i < amount; i++)
+        {
+            Vector3 spawnPos = new Vector3
+            (
+                transform.position.x + Random.Range(-transform.localScale.x / 2, transform.localScale.x / 2),
+                transform.position.y + Random.Range(-transform.localScale.y / 2, transform.localScale.y / 2),
+                transform.position.z + Random.Range(-transform.localScale.z / 2, transform.localScale.z / 2)
+            );
+            GameObject newlySpawned = Instantiate(spawnPrefab, spawnPos, transform.rotation);
+
+            if (newlySpawned.GetComponent<CivilianNavigation>() != null)
+                newlySpawned.GetComponent<CivilianNavigation>().gameObjectManager = gameObjectManager;
+        }
+
+        Destroy(gameObject);
+    }
+}
