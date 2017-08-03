f92254740eb78626d1e33b58b513f865e7786206
James da Silva <james.dasilva1111@gmail.com>
2017-08-04 09:28:14 +1000

    Basic barrier health bars

--- Assets/Scripts/HUDScript.cs
+++ Assets/Scripts/HUDScript.cs
@@ -10,13 +10,18 @@ public class HUDScript : MonoBehaviour
     public Text civiliansSaved;
 
     public GameObject barricadeHealthBarPrefab;
+    List<GameObject> barricades;
     List<GameObject> barricadeHealthBars;
 
 	void Start ()
     {
-        foreach (GameObject barricade in manager.barricades)
+        barricades = manager.barricades;
+        barricadeHealthBars = new List<GameObject>();
+        foreach (GameObject barricade in barricades)
         {
-            barricadeHealthBars.Add(Instantiate(barricadeHealthBarPrefab));
+            GameObject newHealthBar = Instantiate(barricadeHealthBarPrefab) as GameObject;
+            newHealthBar.transform.parent = transform;
+            barricadeHealthBars.Add(newHealthBar);
         }
     }
 	
@@ -25,9 +30,20 @@ public class HUDScript : MonoBehaviour
         timer.text = " " + (Mathf.FloorToInt(manager.timer) / 60).ToString() + ":" + Mathf.FloorToInt(manager.timer % 60).ToString();
         civiliansSaved.text = manager.civiliansEscaped.ToString() + " ";
 
-        foreach (GameObject barricade in manager.barricades)
+        for (int i = barricades.Count - 1; i >= 0; i--)
         {
-            Camera.main.WorldToScreenPoint(barricade.transform.position);
+            if (barricades[i] == null)
+            {
+                barricades.RemoveAt(i);
+                Destroy(barricadeHealthBars[i]);
+                barricadeHealthBars.RemoveAt(i);
+            }
+        }
+
+        for (int i = 0; i < manager.barricades.Count; i++)
+        {
+            barricadeHealthBars[i].transform.position = Camera.main.WorldToScreenPoint(manager.barricades[i].transform.position);
+            barricadeHealthBars[i].GetComponent<Image>().fillAmount = manager.barricades[i].GetComponent<Health>().health / (float)manager.barricades[i].GetComponent<Health>().maxHealth;
         }
 	}
 }
