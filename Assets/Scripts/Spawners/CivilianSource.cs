dd0ad960ac9afd9141e4a0bccb1de7a7a9147a6c
James da Silva <james.dasilva1111@gmail.com>
2017-08-10 11:26:13 +1000

    Script folder rearrangement

---
+++ Assets/Scripts/Spawners/CivilianSource.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CivilianSource : MonoBehaviour
+{
+    public int civilians;
+    int currentCivilians;
+    
+    public float spawnInterval;
+    float spawnTimer;
+
+    bool active;
+
+	void Start ()
+    {
+        spawnTimer = 0;
+        active = false;
+	}
+	
+	void Update ()
+    {
+        spawnTimer -= Time.deltaTime;
+    }
+
+    public void SetSpawning(bool spawn)
+    {
+        active = spawn;
+    }
+
+    public void Reset()
+    {
+        currentCivilians = civilians;
+        spawnTimer = spawnInterval;
+    }
+}
