140dacc8c97a8d4443da52792d18d7dbb2356784
James da Silva <james.dasilva1111@gmail.com>
2017-08-24 11:11:19 +1000

    Initial grenade implementation
    Needs work

---
+++ Assets/Scripts/Player/Player Weapons/LaunchedGrenade.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaunchedGrenade : MonoBehaviour
+{
+    [Tooltip("Time until explosion")]
+    public float time;
+
+    public float damage;
+
+    public GameObject[] particleEffects;
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+        time -= Time.deltaTime;
+        if (time <= 0)
+            Explode();
+	}
+
+    public void Explode()
+    {
+        foreach (GameObject particleEffect in particleEffects)
+            Instantiate(particleEffect, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
