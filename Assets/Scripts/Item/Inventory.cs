ac8f22c82a1d0e98021ef314dbc069fbaa791348
James da Silva <james.dasilva1111@gmail.com>
2017-09-01 10:31:45 +1000

    Item system exists, not tested

---
+++ Assets/Scripts/Item/Inventory.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    PassiveItem passiveItem;
+    ActiveItem activeItem;
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+		
+	}
+
+    public void PickUp(ItemType item)
+    {
+        switch (item)
+        {
+            case ItemType.Test:
+                if (passiveItem != null)
+                    Instantiate(passiveItem.pickupPrefab, transform.position, transform.rotation);
+                passiveItem = new Items.Test();
+                break;
+        }
+    }
+}
