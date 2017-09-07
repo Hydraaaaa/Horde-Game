e13367651b18a18fece2bade1db4cd9781805d6c
James da Silva <james.dasilva1111@gmail.com>
2017-09-08 09:34:41 +1000

    Fixed collider error
    halfway to projectile toggle

--- Assets/Scripts/Player/Player Weapons/Rifle.cs
+++ Assets/Scripts/Player/Player Weapons/Rifle.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 [RequireComponent(typeof(LineRenderer))]
 public class Rifle : MonoBehaviour
 {
+    public bool projectileToggle;
+
+    public GameObject HitIndicatorPrefab;
+    public LineRenderer tracerPrefab;
     public Bullet bulletPrefab;
 
     [Tooltip("Time in seconds between shots")]
@@ -16,6 +20,8 @@ public class Rifle : MonoBehaviour
 
     public float laserLength;
 
+
+    public float range;
     public float projectileSpeed;
 
     public float energyCost;
@@ -73,15 +79,57 @@ public class Rifle : MonoBehaviour
             currentCooldown = cooldown;
             energy -= energyCost;
 
-            Vector3 aimDir = (laserStartPoint.transform.right + Random.insideUnitSphere * accuracy) * projectileSpeed;
+            if (projectileToggle)
+            {
+                Vector3 aimDir = (laserStartPoint.transform.right + Random.insideUnitSphere * accuracy) * projectileSpeed;
 
-            GameObject bullet = Instantiate(bulletPrefab.gameObject, laserStartPoint.transform.position, laserStartPoint.transform.rotation);
-            Bullet bulletScript = bullet.GetComponent<Bullet>();
-            bulletScript.velocity = aimDir;
-            bulletScript.damage = damage;
-            Physics.IgnoreCollision(bullet.GetComponent<Collider>(), GetComponent<SphereCollider>());
-            Physics.IgnoreCollision(bullet.GetComponent<Collider>(), GetComponent<CapsuleCollider>());
+                GameObject bullet = Instantiate(bulletPrefab.gameObject, laserStartPoint.transform.position, laserStartPoint.transform.rotation);
+                Bullet bulletScript = bullet.GetComponent<Bullet>();
+                bulletScript.velocity = aimDir;
+                bulletScript.damage = damage;
 
+                Collider[] playerColliders = GetComponents<Collider>();
+                for (int i = 0; i < playerColliders.Length; i++)
+                    Physics.IgnoreCollision(bullet.GetComponent<Collider>(), playerColliders[i]);
+            }
+            else
+            {
+                Vector3 aimDir = laserStartPoint.transform.right + Random.insideUnitSphere * accuracy;
+
+
+                Ray shootRay = new Ray(laserStartPoint.transform.position, aimDir);
+                RaycastHit hit;
+
+
+                int mask = ~(1 << LayerMask.NameToLayer("CursorRaycast"));
+
+                if (Physics.Raycast(shootRay, out hit, range, mask))
+                {
+                    GameObject newTracer = Instantiate(tracerPrefab.gameObject);
+                    LineRenderer tracerRenderer = newTracer.GetComponent<LineRenderer>();
+                    tracerRenderer.SetPosition(0, laserStartPoint.transform.position);
+                    tracerRenderer.SetPosition(1, hit.point);
+
+                    Instantiate(HitIndicatorPrefab, hit.point, transform.rotation);
+
+                    if (hit.transform.GetComponent<Health>() != null)
+                    {
+                        hit.transform.GetComponent<Health>().Damage(damage);
+
+                        // If the attacked target is an enemy
+                        if (hit.transform.GetComponent<Health>().Enemy && this.CompareTag("Player"))
+                            hit.transform.GetComponent<Health>().Attacker = this.gameObject;
+                    }
+                }
+                else
+                {
+                    GameObject newTracer = Instantiate(tracerPrefab.gameObject);
+                    LineRenderer tracerRenderer = newTracer.GetComponent<LineRenderer>();
+                    tracerRenderer.SetPosition(0, laserStartPoint.transform.position);
+                    tracerRenderer.SetPosition(1, laserStartPoint.transform.position + aimDir * range);
+                }
+            }
+                
             if (gunshot != null)
                 gunshot.Play();
         }
