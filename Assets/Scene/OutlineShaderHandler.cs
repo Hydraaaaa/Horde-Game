1804395e7168e5f663873d7e11fdece61d100322
Josh <joshuacorney2@gmail.com>
2017-09-07 15:06:47 +1000

    small fixes, some changes

---
+++ Assets/Scene/OutlineShaderHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutlineShaderHandler : MonoBehaviour
+{
+    public Color BaseColor;
+    public Color OutlineColor;
+    public float OutlineWidth;
+    public Texture texture;
+    public float Speed = 1, Offset;
+
+    private Renderer _renderer;
+    private MaterialPropertyBlock _propBlock;
+
+    void Awake()
+    {
+        _propBlock = new MaterialPropertyBlock();
+        _renderer = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        //// Get the current value of the material properties in the renderer.
+        //_renderer.GetPropertyBlock(_propBlock);
+
+        //// Assign our new value.
+        //_propBlock.SetColor("_Color", BaseColor);
+        //_propBlock.SetColor("_OutlineColor", OutlineColor);
+        //_propBlock.SetFloat("_Outline", OutlineWidth);
+        //_propBlock.SetTexture("_MainTex", texture);
+
+        //// Apply the edited values to the renderer.
+        //_renderer.SetPropertyBlock(_propBlock);
+    }
+}
