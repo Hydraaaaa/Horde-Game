7df4325ac09d24b12b3dd3c533498a4952028ca4
James da Silva <james.dasilva1111@gmail.com>
2017-09-13 12:17:44 +1000

    Fixed an error

--- Assets/Scripts/Player/PlayerInit.cs
+++ Assets/Scripts/Player/PlayerInit.cs
@@ -10,20 +10,7 @@ public class PlayerInit : MonoBehaviour
     {
         GameObject aimRaycastBox = Instantiate(aimRaycastBoxPrefab);
         aimRaycastBox.GetComponent<SetPosition>().target = gameObject;
-        GetComponent<Health>().OnDie = PlayerDie;
         
         Destroy(this);
 	}
-
-    public void PlayerDie()
-    {
-        for (int i = 0; i < GameObjectManager.instance.players.Count; i++)
-        {
-            if (GameObjectManager.instance.players[i].gameObject == gameObject)
-            {
-                GameObjectManager.instance.players[i].gameObject = null;
-            }
-        }
-        Destroy(gameObject);
-    }
 }
